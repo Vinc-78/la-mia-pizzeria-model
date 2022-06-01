23bab3530ef8d6af339fc10a86e8ed12fd219d67
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-06-01 18:55:37 +0200

    Aggiungere i file di progetto.

---
+++ Models/Pizza.cs
@@ -0,0 +1,25 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+   
+    public class Pizza
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string? Descrizione { get; set; }
+
+        public string? ImgPath { get; set; }
+
+        public string Prezzo { get; set; }
+
+        
+    }
+
+    public class listaPizze {
+
+        public List<Pizza> pizzas { get; set; }
+
+        public listaPizze() {
+          pizzas = new List<Pizza>();
+    }
+    }
+}
