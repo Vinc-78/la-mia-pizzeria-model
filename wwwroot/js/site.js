41be1048752b53fe3bf52423acf2c5f1d315578e
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-06-03 14:23:29 +0200

    Add hover con jquery

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('.imgIndex').hover
+(
+    function ()
+    {
+        $(this).animate({ 'zoom': 1.2 }, 700); //questa parte ingrandisce in 700 millisecondi
+    },
+    function ()
+    {
+        $(this).animate({ 'zoom': 1 }, 400);
+    }
+)
